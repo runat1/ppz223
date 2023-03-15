e84062335a6a8fcdfd134375567ec23411e0417f
runat1 <121800602+runat1@users.noreply.github.com>
2023-03-15 22:03:45 +0500

    Delete check directory

--- check/src/main/java/org/example/Cat.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Cat implements Pet{
-    @Override
-    public void sey() {
-        System.out.println("mov-mov");
-    }
-}
--- check/src/main/java/org/example/Dog.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Dog implements Pet{
-    @Override
-    public void sey(){
-        System.out.println("gav-gav");
-    }
-}
